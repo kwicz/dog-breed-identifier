3d8cc2cda84ec382ab084bf1917b848f5cad27c8
Katy Solovewicz <katy@solovewicz.com>
2020-05-07 15:41:28 -0700

    Updates

---
+++ precache-manifest.d8bfabe2d946e5ed5ed9fb6033279374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275ec28ffe27e9bb4bf00e2d01dfb023",
+    "url": "/dog-breed-identifier/index.html"
+  },
+  {
+    "revision": "06d3fa922fda193a6213",
+    "url": "/dog-breed-identifier/static/css/main.a3cc9738.chunk.css"
+  },
+  {
+    "revision": "bd86c897b49ff77f5b11",
+    "url": "/dog-breed-identifier/static/js/2.afe90166.chunk.js"
+  },
+  {
+    "revision": "613461baf49b56a7d6d6e1742f0456e3",
+    "url": "/dog-breed-identifier/static/js/2.afe90166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d3fa922fda193a6213",
+    "url": "/dog-breed-identifier/static/js/main.8ad5d81f.chunk.js"
+  },
+  {
+    "revision": "945334f17032b85adc7d",
+    "url": "/dog-breed-identifier/static/js/runtime-main.51107cba.js"
+  }
+]);
